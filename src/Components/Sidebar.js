9a4cfb52ae12e544fb4b49e85242deb3a0684737
Laviesha <122373382+Laviesha@users.noreply.github.com>
2023-06-30 20:05:18 +0530

    Update Sidebar.js

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,30 +1,43 @@
 import React from 'react'
-import './Sidebar.css'
+import SidebarRow from './BarRow'
 import HomeIcon from '@mui/icons-material/Home';
 import ExploreIcon from '@mui/icons-material/Explore';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import VideoLibraryOutlinedIcon from '@mui/icons-material/VideoLibraryOutlined';
+import HistoryOutlinedIcon from '@mui/icons-material/HistoryOutlined';
+import WatchLaterOutlinedIcon from '@mui/icons-material/WatchLaterOutlined';
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+import SettingsTwoToneIcon from '@mui/icons-material/SettingsTwoTone';
+import FlagTwoToneIcon from '@mui/icons-material/FlagTwoTone';
+import HelpTwoToneIcon from '@mui/icons-material/HelpTwoTone';
+import AnnouncementTwoToneIcon from '@mui/icons-material/AnnouncementTwoTone';
 
-
+import "./Sidebar.css"
 
 function Sidebar() {
   return (
     <div className='sidebar'>
-      <div className='home'>
-      <HomeIcon/>
-      <span>Home</span>
-      </div>
+    <SidebarRow selected Icon={HomeIcon} title="Home"/>
+    <SidebarRow Icon={ExploreIcon} title="Explore"/>
+    <SidebarRow Icon={SubscriptionsIcon} title="Subscription"/>
+    <hr /> 
+    <SidebarRow Icon={VideoLibraryOutlinedIcon} title="Library"/>
+    <SidebarRow Icon={ HistoryOutlinedIcon} title="History"/>
+    <SidebarRow Icon={WatchLaterOutlinedIcon} title="Watch Later"/>
+    <SidebarRow Icon={FileDownloadOutlinedIcon} title="Downloads"/>
+    <SidebarRow Icon={ThumbUpOutlinedIcon} title="Liked videos"/>
+    <SidebarRow Icon={KeyboardArrowDownOutlinedIcon} title="Show More"/>
+    <hr/>
+    <SidebarRow Icon={SettingsTwoToneIcon} title="Settings"/>
+    <SidebarRow Icon={FlagTwoToneIcon} title="Report History"/>
+    <SidebarRow Icon={HelpTwoToneIcon} title="Help"/>
+    <SidebarRow Icon={AnnouncementTwoToneIcon} title="Send Feedback"/>
 
-      <div className='explore'>
-      <ExploreIcon/>
-      <span>Explore</span>
-      </div>
-
-      <div className='subs'>
-      <SubscriptionsIcon/>
-      <span>Subsxriptions</span>
-    </div>
     </div>
   )
 }
 
 export default Sidebar
+
